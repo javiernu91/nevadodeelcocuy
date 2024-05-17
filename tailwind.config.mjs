ef239fb60c52e6b4db98eb55b661aa0714ec44f4
Javier Núñez <javiernu16@gmail.com>
2024-05-17 16:48:29 -0500

    Add logo and create personaliced colors

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,17 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
+		colors: {
+			lightBg: '#FDFDFF',
+			lightText: '#19191C',
+			lightBorder: '#C0E9D1',
+			darkBg: '#19191C',
+			darkText: '#FDFDFF'
+			
+
+		},
 		extend: {},
 	},
 	plugins: [],
+	darkMode: 'class'
 }
