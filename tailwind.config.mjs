d4c2c34100a11ac3440b7e53044f212722fca133
Javier Núñez <javiernu16@gmail.com>
2024-12-07 13:51:40 -0500

    Add tailwind typography plugin to fix the md styles

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,18 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		colors: {
-			lightBg: '#FDFDFF',
-			lightText: '#19191C',
-			lightBorder: '#C0E9D1',
-			darkBg: '#19191C',
-			darkText: '#FDFDFF'
-			
-
-		},
-		extend: {},
-	},
-	plugins: [],
-	darkMode: 'class'
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    colors: {
+      lightBg: "#FDFDFF",
+      lightText: "#19191C",
+      lightBorder: "#C0E9D1",
+      darkBg: "#19191C",
+      darkText: "#FDFDFF",
+    },
+    extend: {},
+  },
+  plugins: [require("@tailwindcss/typography")],
+  darkMode: "class",
+};
